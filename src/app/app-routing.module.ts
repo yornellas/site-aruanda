1f37ec8b23f3300e26905ba4a842bea8c61edfa4
yornellas <yo_ornellas@hotmail.com>
2023-03-21 12:15:31 -0300

    chore: rearange components and add link to about us section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
-const routes: Routes = [];
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64],
+};
+
+const routes: Routes = [
+  { 
+    path: 'pagina-inicial', 
+    component: MainPageComponent,
+    children: [
+      {
+        path: 'sobre',
+        component: AboutUsComponent
+      }
+    ]
+  },
+  { path: '', redirectTo: '/pagina-inicial', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
