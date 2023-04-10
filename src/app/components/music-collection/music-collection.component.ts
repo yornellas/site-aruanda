948b2a27f46586e2b2983b6545794b28075e22f3
yornellas <yo_ornellas@hotmail.com>
2023-04-10 14:19:10 -0300

    feat: create music collection component

---
+++ src/app/components/music-collection/music-collection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'music-collection',
+  templateUrl: './music-collection.component.html',
+  styleUrls: ['./music-collection.component.scss']
+})
+export class MusicCollectionComponent {
+
+}
