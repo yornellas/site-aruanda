e00b2e1596417ff1bdfa07319665dc9f610ff51f
yornellas <yo_ornellas@hotmail.com>
2023-04-11 08:35:47 -0300

    feat: add Norte music list

---
+++ src/app/components/music-collection/norte/norte.ts
@@ -0,0 +1,27 @@
+export const Norte = [
+  {
+    music_name: 'Xote Bragantino Moreninha',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/243138430&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+    pdf_url: 'https://yornellas.github.io/aruanda-music-docs/norte/XOTE_BRAGANTINO%20MORENINHA.pdf'
+  },
+  {
+    music_name: 'Xote Bragantino',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/243138430&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+    pdf_url: 'https://yornellas.github.io/aruanda-music-docs/norte/Xote_Bragantino.pdf'
+  },
+  {
+    music_name: 'Vaqueiros do Marajó',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/243138430&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+    pdf_url: 'https://yornellas.github.io/aruanda-music-docs/norte/VAQUEIROS_DO_MARAJ%C3%93.pdf'
+  },
+  {
+    music_name: 'Retumbão',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/243138430&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+    pdf_url: 'https://yornellas.github.io/aruanda-music-docs/norte/RETUMB%C3%83O.pdf'
+  },
+  {
+    music_name: 'Pescador - Olha o Peru',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/243138430&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+    pdf_url: 'https://yornellas.github.io/aruanda-music-docs/norte/Carimb%C3%B3_Pescador.pdf'
+  },
+]
