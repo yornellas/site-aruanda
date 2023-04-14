6e4588e47fc92a14f868535f3a61d497f298eb53
yornellas <yo_ornellas@hotmail.com>
2023-04-14 10:14:08 -0300

    chore: add type to const

--- src/app/components/music-collection/norte/norte.ts
+++ src/app/components/music-collection/norte/norte.ts
@@ -1,4 +1,6 @@
-export const Norte = [
+import { MusicListInterface } from "src/app/shared/interfaces/music-list.interface";
+
+export const Norte: MusicListInterface[] = [
   {
     music_name: 'Xote Bragantino Moreninha',
     audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/243138430&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
