11b1027af49c41113d255dbb7d610c338048e639
yornellas <yo_ornellas@hotmail.com>
2023-04-17 17:14:58 -0300

    chore: add audio links

--- src/app/components/music-collection/sudeste/sudeste.ts
+++ src/app/components/music-collection/sudeste/sudeste.ts
@@ -3,127 +3,133 @@ import { MusicListInterface } from "src/app/shared/interfaces/music-list.interfa
 export const Sudeste: MusicListInterface[] = [
   {
     music_name: 'Boa Noite Meus Senhores',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494731770&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/BOA_NOITE_MEUS_SENHORES.pdf',
   },
   {
     music_name: 'Boi-Lé',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494729394&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/BOI-LE.pdf',
   },
   {
     music_name: 'Caiapó',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494734848&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/CAIAPO.pdf',
   },
   {
     music_name: 'Carneiro',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494735538&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/CARNEIRO.pdf',
   },
   {
     music_name: 'Catira',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494736054&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/CATIRA.pdf',
   },
   {
     music_name: 'Catopê',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494736504&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/CATOPE.pdf',
   },
   {
     music_name: 'Ciganinhas',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494737044&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/CIGANINHAS.pdf',
   },
   {
     music_name: 'Congada Mineira',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494737581&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/CONGADA_MINEIRA.pdf',
   },
   {
     music_name: 'Congo Penacho Real',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494738238&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/CONGO_PENACHO_REAL.pdf',
   },
   {
     music_name: 'Dança das Fitas',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494739045&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/Danca_das_fitas.pdf',
   },
   {
     music_name: 'Facão Guarani',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494763570&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/FACAO_GUARANI.pdf',
+    extra_video_url: 'https://www.youtube.com/embed/rrwOuMB1e5U',
+    extra_pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/FACAO_GUARANI-ViolaCaipira.pdf'
   },
   {
     music_name: 'Folia de Reis',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494742924&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/FOLIA_DE_REIS.pdf',
   },
   {
     music_name: 'Folia do Divino',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494742522&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/FOLIA_DO_DIVINO.pdf',
   },
   {
     music_name: 'Hino do Aruanda',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494728200&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/HINO_DO_ARUANDA.pdf',
   },
   {
     music_name: 'Moçambiqueiro',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494743644&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/MOCAMBIQUEIRO.pdf',
   },
   {
     music_name: 'Mutum',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494744097&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/MUTUM.pdf',
+    extra_video_url: 'https://www.youtube.com/embed/DRh6KIsMSJM',
+    extra_pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/MUTUM-ViolaCaipira.pdf'
   },
   {
     music_name: 'Pastoril do Barreiro',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494750268&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/PASTORIL_DO_BARREIRO.pdf',
   },
   {
     music_name: 'Pastorinhas',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494734332&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/PASTORINHAS.pdf',
   },
   {
     music_name: 'Rainha Conga',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494755866&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/RAINHA_CONGA.pdf',
   },
   {
     music_name: 'Santo Reis',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494756373&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/SANTO_REIS.pdf',
+    extra_video_url: 'https://www.youtube.com/embed/y3T9p3F7Wsw',
+    extra_pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/SANTO_REIS-ViolaCaipira.pdf'
   },
   {
     music_name: 'São Gonçalo',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494756862&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/SÃO_GONCALO.pdf',
   },
   {
     music_name: 'Tamboril',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494758431&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/TAMBORIL.pdf',
   },
   {
     music_name: 'Terno de São Benedito',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494730108&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/TERNO_DE_SAO_BENEDITO.pdf',
   },
   {
     music_name: 'Vilão de Facas',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494762853&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/VILAO_DE_FACAS.pdf',
   },
   {
     music_name: 'Visita ao Reino de Moçambique',
-    audio_url: '',
+    audio_url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494728836&color=%23100b0e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     pdf_url: 'https://yornellas.github.io/aruanda-music-docs/sudeste/VISITA_AO_REINO_DE_MOCAMBIQUE.pdf',
   }
 ]
