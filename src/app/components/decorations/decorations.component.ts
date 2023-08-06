1f9cc5ae755059b407ee4a2b1b5174d700611c91
yornellas <yo_ornellas@hotmail.com>
2023-08-06 12:17:09 -0300

    chore: add separate file to other decorations

--- src/app/components/decorations/decorations.component.ts
+++ src/app/components/decorations/decorations.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GalleryItem, ImageItem } from 'ng-gallery';
-import { Decorations } from './decorations';
+import { MainDecorations } from './main-decorations';
+import { OtherDecorations } from './other-decorations';
 
 @Component({
   selector: 'decorations',
@@ -8,16 +9,21 @@ import { Decorations } from './decorations';
   styleUrls: ['./decorations.component.scss']
 })
 export class DecorationsComponent implements OnInit {
-  decorations: GalleryItem[];
-  title: string = Decorations[0].title
+  mainDecorations: GalleryItem[];
+  otherDecorations: GalleryItem[];
+  title: string = MainDecorations[0].title
 
   ngOnInit() {
-    this.decorations = Decorations.map(photo =>
+    this.mainDecorations = MainDecorations.map(photo =>
+      new ImageItem({src: photo.srcUrl})
+    )
+
+    this.otherDecorations = OtherDecorations.map(photo =>
       new ImageItem({src: photo.srcUrl})
     )
   }
 
   setTitle(event) {
-    this.title = Decorations[event.currIndex].title
+    this.title = MainDecorations[event.currIndex].title
   }
 }
--- src/app/components/decorations/main-decorations.ts
+++ src/app/components/decorations/main-decorations.ts
@@ -1,9 +1,8 @@
 import { PhotoCollectionInterface } from "src/app/shared/interfaces/photo-collection.interface";
 
-export const Decorations: PhotoCollectionInterface[] = [
+export const MainDecorations: PhotoCollectionInterface[] = [
   {
     srcUrl: '../../assets/images/decorations/ordem-do-merito-cultural-2010.jpg',
-    previewUrl: '../../assets/images/decorations/thumbs/ordem-do-merito-cultural-2010_tn.jpg',
     title: `<h4 class="text-roboto-bold">Ordem do Mérito Cultural</h4>
             <p class="text-roboto">
             Decreto de 01 de dezembro de 2010 por suas relevantes contribuições à cultura 
@@ -13,7 +12,6 @@ export const Decorations: PhotoCollectionInterface[] = [
   },
   {
     srcUrl: '../../assets/images/decorations/ordem-do-merito-legislativo-2010.jpg',
-    previewUrl: '../../assets/images/decorations/thumbs/ordem-do-merito-legislativo-2010_tn.jpg',
     title: `<h4 class="text-roboto-bold">Ordem do Mérito Legislativo</h4>
             <p class="text-roboto">Admissão como membro da Ordem do Mérito Legislativo, 
             no grau mérito Resolução nº 2778, de 27 de abril de 1982.<br>
@@ -21,14 +19,12 @@ export const Decorations: PhotoCollectionInterface[] = [
   },
   {
     srcUrl: '../../assets/images/decorations/medalha-da-inconfidencia-2011.jpg',
-    previewUrl: '../../assets/images/decorations/thumbs/medalha-da-inconfidencia-2011_tn.jpg',
     title: `<h4 class="text-roboto-bold">Medalha de Honra da Inconfidência</h4>
             <p class="text-roboto">Lei nº 882, de 28 de julho de 1952.<br>
             Concedida pelo governador do Estado de Minas Gerais.</p>`
   },
   {
     srcUrl: '../../assets/images/decorations/merito-legislativo-municipal-2010.jpg',
-    previewUrl: '../../assets/images/decorations/thumbs/merito-legislativo-municipal-2010_tn.jpg',
     title: `<h4 class="text-roboto-bold">Grande Colar do Mérito Legislativo Municipal</h4>
             <p class="text-roboto">
             Assinado pelo presidente da Câmara de Vereadores de Belo Horizonte.
