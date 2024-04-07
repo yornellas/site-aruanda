cb75fcffb8dee111bb9e76d0eb8a08a559bd15f3
yornellas <yo_ornellas@hotmail.com>
2024-04-07 18:12:14 -0300

    feat: add news

--- src/app/components/news/news.ts
+++ src/app/components/news/news.ts
@@ -3,11 +3,16 @@ import { NewsInterface } from "src/app/shared/interfaces"
 const DEFAULT_URL = 'assets/images/news/'
 
 export const News: NewsInterface[] =[
-  // {
-  //   url: `${DEFAULT_URL}`,
-  //   href: 'https://portalbelohorizonte.com.br/eventos/espetaculo/danca/espetaculo-festa-e-de-aruanda',
-  //   text: 'Espetáculo: “A Festa é de Aruanda”'
-  // },
+  {
+    url: `${DEFAULT_URL}casa-cheia.png`,
+    href: 'https://bhaz.com.br/noticias/bh/grupo-aruanda-programacao-cultural-gratuita-bh/',
+    text: 'Projeto Casa Cheia: Grupo Aruanda tem programação cultural gratuita em Belo Horizonte”'
+  },
+  {
+    url: `${DEFAULT_URL}festa-de-aruanda-palacio-das-artes.png`,
+    href: 'https://portalbelohorizonte.com.br/eventos/espetaculo/danca/espetaculo-festa-e-de-aruanda',
+    text: 'Espetáculo: “A Festa é de Aruanda”'
+  },
   {
     url: `${DEFAULT_URL}danca-caboclos.png`,
     href: 'https://agorarn.com.br/ultimas/danca-dos-caboclos-do-rio-grande-do-norte-e-incorporada-ao-repertorio-de-grupo-de-cultura-popular/',
@@ -29,14 +34,14 @@ export const News: NewsInterface[] =[
     text: '2º Circuito Mineiro de Dança - 2022'
   },
   {
-    url: `${DEFAULT_URL}pbh-teatro-francisco-nunes.png`,
-    href: 'https://prefeitura.pbh.gov.br/noticias/grupo-folclorico-aruanda-se-apresenta-no-teatro-francisco-nunes',
-    text: 'Grupo Folclórico Aruanda se apresenta no Teatro Francisco Nunes'
-  },
-  {
     url: `${DEFAULT_URL}aruanda-na-europa.png`,
     href: 'https://www.sinprominas.org.br/grupo-aruanda-vai-representar-o-brasil-na-europa/',
     text: 'Grupo Aruanda vai representar o Brasil na Europa'
+  },
+  {
+    url: `${DEFAULT_URL}pbh-teatro-francisco-nunes.png`,
+    href: 'https://prefeitura.pbh.gov.br/noticias/grupo-folclorico-aruanda-se-apresenta-no-teatro-francisco-nunes',
+    text: 'Grupo Folclórico Aruanda se apresenta no Teatro Francisco Nunes'
   },
   {
     url: `${DEFAULT_URL}aruanda-em-itauna.png`,
@@ -44,14 +49,14 @@ export const News: NewsInterface[] =[
     text: 'Grupo Aruanda apresenta danças brasileiras no Sílvio de Mattos'
   },
   {
-    url: `${DEFAULT_URL}aruanda-estrada-real.jpg`,
-    href: 'https://ufop.br/eventos/espetaculo-do-grupo-aruanda',
-    text: 'Espetáculo do Grupo Aruanda'
-  },
-  {
     url: `${DEFAULT_URL}aruanda-50-anos.png`,
     href: 'https://www.hojeemdia.com.br/entretenimento/grupo-aruanda-traz-espetaculo-exposic-o-e-livro-comemorativo-aos-seus-50-anos-de-fundac-o-1.373850',
     text: 'Grupo Aruanda traz espetáculo, exposição e livro comemorativo aos seus 50 anos de fundação'
+  },
+  {
+    url: `${DEFAULT_URL}aruanda-estrada-real.jpg`,
+    href: 'https://ufop.br/eventos/espetaculo-do-grupo-aruanda',
+    text: 'Espetáculo do Grupo Aruanda'
   },
   {
     url: `${DEFAULT_URL}museu-da-moda.png`,
