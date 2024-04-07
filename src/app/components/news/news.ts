9797927639fe91c5b1cbc8f3e937dd955edb6c3a
yornellas <yo_ornellas@hotmail.com>
2024-04-07 16:35:57 -0300

    feat: add news information

--- src/app/components/news/news.ts
+++ src/app/components/news/news.ts
@@ -3,20 +3,89 @@ import { NewsInterface } from "src/app/shared/interfaces"
 const DEFAULT_URL = 'assets/images/news/'
 
 export const News: NewsInterface[] =[
-  {url: `${DEFAULT_URL}danca-caboclos.png`},
-  {url: `${DEFAULT_URL}festival-vitoria-es.png`},
-  {url: `${DEFAULT_URL}viraliza-bh.png`},
-  {url: `${DEFAULT_URL}circuito-mineiro-danca.jpg`},
-  {url: `${DEFAULT_URL}pbh-teatro-francisco-nunes.png`},
-  {url: `${DEFAULT_URL}aruanda-na-europa.png`},
-  {url: `${DEFAULT_URL}aruanda-em-itauna.png`},
-  {url: `${DEFAULT_URL}aruanda-estrada-real.jpg`},
-  {url: `${DEFAULT_URL}aruanda-50-anos.png`},
-  {url: `${DEFAULT_URL}museu-da-moda.png`},
-  {url: `${DEFAULT_URL}aruanda-55-anos-memorial-minas-gerais.png`},
-  {url: `${DEFAULT_URL}aruanda-50-anos-conservatorio.png`},
-  {url: `${DEFAULT_URL}aruanda-em-contagem.png`},
-  {url: `${DEFAULT_URL}fundacao-municipal-de-cultura.png`},
-  {url: `${DEFAULT_URL}crmoda.png`},
-  {url: `${DEFAULT_URL}teatro-raul-belem-machado.png`},
+  // {
+  //   url: `${DEFAULT_URL}`,
+  //   href: 'https://portalbelohorizonte.com.br/eventos/espetaculo/danca/espetaculo-festa-e-de-aruanda',
+  //   text: 'Espetáculo: “A Festa é de Aruanda”'
+  // },
+  {
+    url: `${DEFAULT_URL}danca-caboclos.png`,
+    href: 'https://agorarn.com.br/ultimas/danca-dos-caboclos-do-rio-grande-do-norte-e-incorporada-ao-repertorio-de-grupo-de-cultura-popular/',
+    text: 'Dança dos Caboclos do Rio Grande do Norte é incorporada ao repertório de grupo de cultura popular'
+  },
+  {
+    url: `${DEFAULT_URL}festival-vitoria-es.png`,
+    href: 'https://eshoje.com.br/2022/05/vitoria-recebe-festival-de-dancas-populares-com-grupos-capixabas-e-nacionais/',
+    text: 'Vitória recebe festival de danças populares com grupos capixabas e nacionais'
+  },
+  {
+    url: `${DEFAULT_URL}viraliza-bh.png`,
+    href: 'https://viralizabh.com.br/bh-que-encanta-finaliza-a-primeira-edicao-com-show-do-grupo-aruanda-lancamento-de-livros-e-muito-mais/',
+    text: 'BH que Encanta finaliza a primeira edição com show do Grupo Aruanda, lançamento de livros e muito mais'
+  },
+  {
+    url: `${DEFAULT_URL}circuito-mineiro-danca.jpg`,
+    href: 'https://www.studioit.art.br/circuitomineirodedanca/',
+    text: '2º Circuito Mineiro de Dança - 2022'
+  },
+  {
+    url: `${DEFAULT_URL}pbh-teatro-francisco-nunes.png`,
+    href: 'https://prefeitura.pbh.gov.br/noticias/grupo-folclorico-aruanda-se-apresenta-no-teatro-francisco-nunes',
+    text: 'Grupo Folclórico Aruanda se apresenta no Teatro Francisco Nunes'
+  },
+  {
+    url: `${DEFAULT_URL}aruanda-na-europa.png`,
+    href: 'https://www.sinprominas.org.br/grupo-aruanda-vai-representar-o-brasil-na-europa/',
+    text: 'Grupo Aruanda vai representar o Brasil na Europa'
+  },
+  {
+    url: `${DEFAULT_URL}aruanda-em-itauna.png`,
+    href: 'https://www.itauna.mg.gov.br/portal/noticias/0/3/1110/grupo-aruanda-apresenta-dancas-brasileiras-no-silvio-de-mattos',
+    text: 'Grupo Aruanda apresenta danças brasileiras no Sílvio de Mattos'
+  },
+  {
+    url: `${DEFAULT_URL}aruanda-estrada-real.jpg`,
+    href: 'https://ufop.br/eventos/espetaculo-do-grupo-aruanda',
+    text: 'Espetáculo do Grupo Aruanda'
+  },
+  {
+    url: `${DEFAULT_URL}aruanda-50-anos.png`,
+    href: 'https://www.hojeemdia.com.br/entretenimento/grupo-aruanda-traz-espetaculo-exposic-o-e-livro-comemorativo-aos-seus-50-anos-de-fundac-o-1.373850',
+    text: 'Grupo Aruanda traz espetáculo, exposição e livro comemorativo aos seus 50 anos de fundação'
+  },
+  {
+    url: `${DEFAULT_URL}museu-da-moda.png`,
+    href: 'https://prefeitura.pbh.gov.br/noticias/exposicao-no-museu-da-moda-apresenta-trajes-de-manifestacoes-do-folclore-mineiro',
+    text: 'Exposição no Museu da Moda apresenta trajes de manifestações do folclore mineiro'
+  },
+  {
+    url: `${DEFAULT_URL}aruanda-50-anos-conservatorio.png`,
+    href: 'https://www.ufmg.br/online/arquivos/016508.shtml',
+    text: 'Grupo Aruanda comemora 50 anos com apresentação neste domingo na entrada do Conservatório'
+  },
+  {
+    url: `${DEFAULT_URL}aruanda-55-anos-memorial-minas-gerais.png`,
+    href: 'https://entretenimento.uol.com.br/noticias/redacao/2016/04/15/grupo-aruanda-comemora-55-anos-com-exposicao-no-memorial-minas-gerais.htm',
+    text: 'Grupo Aruanda comemora 55 anos com exposição no Memorial Minas Gerais'
+  },
+  {
+    url: `${DEFAULT_URL}crmoda.png`,
+    href: 'https://soubh.uai.com.br/noticias/viva-bem/crmoda-mostra-gratuita-folclore-brasileiro',
+    text: 'CRModa tem mostra gratuita sobre o folclore brasileiro'
+  },
+  {
+    url: `${DEFAULT_URL}fundacao-municipal-de-cultura.png`,
+    href: 'https://prefeitura.pbh.gov.br/noticias/fundacao-municipal-de-cultura-realiza-programacao-especial-no-mes-do-folclore',
+    text: 'Fundação Municipal de Cultura realiza programação especial no mês do folclore'
+  },
+  {
+    url: `${DEFAULT_URL}aruanda-em-contagem.png`,
+    href: 'http://www.diariodecontagem.com.br/Materia/9197/21/grupo-aruanda-traz-espetaculo-sobre-tradicoes-folcloricas-do-pais/',
+    text: 'Grupo Aruanda traz espetáculo sobre tradições folclóricas do país'
+  },
+  {
+    url: `${DEFAULT_URL}teatro-raul-belem-machado.png`,
+    href: 'https://prefeitura.pbh.gov.br/noticias/espetaculo-no-raul-belem-machado-mostra-manifestacoes-culturais-brasileiras',
+    text: 'Espetáculo no Raul Belém Machado mostra as manifestações culturais brasileiras'
+  },
 ]
