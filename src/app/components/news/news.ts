8b1fe0329b16cc065f3a21fff2f62cd62d89571f
yornellas <yo_ornellas@hotmail.com>
2024-04-07 10:56:26 -0300

    feat: add news files

---
+++ src/app/components/news/news.ts
@@ -0,0 +1,22 @@
+import { NewsInterface } from "src/app/shared/interfaces"
+
+const DEFAULT_URL = 'assets/images/news/'
+
+export const News: NewsInterface[] =[
+  {url: `${DEFAULT_URL}danca-caboclos.png`},
+  {url: `${DEFAULT_URL}festival-vitoria-es.png`},
+  {url: `${DEFAULT_URL}viraliza-bh.png`},
+  {url: `${DEFAULT_URL}circuito-mineiro-danca.jpg`},
+  {url: `${DEFAULT_URL}pbh-teatro-francisco-nunes.png`},
+  {url: `${DEFAULT_URL}aruanda-na-europa.png`},
+  {url: `${DEFAULT_URL}aruanda-em-itauna.png`},
+  {url: `${DEFAULT_URL}aruanda-estrada-real.jpg`},
+  {url: `${DEFAULT_URL}aruanda-50-anos.png`},
+  {url: `${DEFAULT_URL}museu-da-moda.png`},
+  {url: `${DEFAULT_URL}aruanda-55-anos-memorial-minas-gerais.png`},
+  {url: `${DEFAULT_URL}aruanda-50-anos-conservatorio.png`},
+  {url: `${DEFAULT_URL}aruanda-em-contagem.png`},
+  {url: `${DEFAULT_URL}fundacao-municipal-de-cultura.png`},
+  {url: `${DEFAULT_URL}crmoda.png`},
+  {url: `${DEFAULT_URL}teatro-raul-belem-machado.png`},
+]
