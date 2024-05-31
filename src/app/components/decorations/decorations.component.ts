047ef6378c0a9ff8a1a100092febf73bf4c7bcd2
yornellas <yo_ornellas@hotmail.com>
2024-05-31 14:53:31 -0300

    refact: decorations

--- src/app/components/decorations/decorations.component.ts
+++ src/app/components/decorations/decorations.component.ts
@@ -1,29 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { GalleryItem, ImageItem } from 'ng-gallery';
+import { Component } from '@angular/core';
+import { faMedal } from '@fortawesome/free-solid-svg-icons';
+import { Decoration } from 'src/app/shared/interfaces';
 import { MainDecorations } from './main-decorations';
-import { OtherDecorations } from './other-decorations';
 
 @Component({
   selector: 'decorations',
   templateUrl: './decorations.component.html',
   styleUrls: ['./decorations.component.scss']
 })
-export class DecorationsComponent implements OnInit {
-  mainDecorations: GalleryItem[];
-  otherDecorations: GalleryItem[];
-  title: string = MainDecorations[0].title
-
-  ngOnInit() {
-    this.mainDecorations = MainDecorations.map(photo =>
-      new ImageItem({src: photo.srcUrl})
-    )
-
-    this.otherDecorations = OtherDecorations.map(photo =>
-      new ImageItem({src: photo.srcUrl})
-    )
-  }
-
-  setTitle(event) {
-    this.title = MainDecorations[event.currIndex].title
-  }
+export class DecorationsComponent {
+  mainDecorations: Decoration[] = MainDecorations;
+  icon = faMedal
 }
