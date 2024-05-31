e959eacd5ae6d90ba20490f727dd18bbdb27a409
yornellas <yo_ornellas@hotmail.com>
2024-05-31 19:11:44 -0300

    feat: add modal to show file

--- src/app/components/decorations/decorations.component.ts
+++ src/app/components/decorations/decorations.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { faMedal } from '@fortawesome/free-solid-svg-icons';
+
 import { Decoration } from 'src/app/shared/interfaces';
 import { MainDecorations } from './main-decorations';
+import { PdfModalComponent } from 'src/app/shared/pdf-modal/pdf-modal.component';
 
 @Component({
   selector: 'decorations',
@@ -11,4 +14,15 @@ import { MainDecorations } from './main-decorations';
 export class DecorationsComponent {
   mainDecorations: Decoration[] = MainDecorations;
   icon = faMedal
+
+  constructor(private modalService: NgbModal) {}
+
+  openPdfModal(url: string, title: string): void {
+    const { componentInstance } = this.modalService.open(PdfModalComponent, {
+      backdrop: true,
+      size: 'lg',
+    })
+    componentInstance.pdfSource = url
+    componentInstance.pdfTitle = title
+  }
 }
