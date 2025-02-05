3e05fb325e32e01f63c8a8b353696640afed2749
yornellas <yo_ornellas@hotmail.com>
2025-02-05 08:00:30 -0300

    feat: add adreements amendment info

--- src/app/components/agreements/agreements.ts
+++ src/app/components/agreements/agreements.ts
@@ -4,6 +4,7 @@ export const Agreements = [
     signed_at: '25/10/2023',
     project_name: 'Pindorama, Terra de Aruanda',
     value: 'R$105.000,00 (cento e cinco mil reais)',
+    amendment: '40640014 | Autoria: Dep. Fed. Rogério Correia',
     accountability_date: '26 de fevereiro de 2026.',
     url: 'https://yornellas.github.io/aruanda-docs/Agreements/PUBLICACAO-TERMO-DE-FOMENTO-943414.pdf'
   },
@@ -12,6 +13,7 @@ export const Agreements = [
     signed_at: '21/12/2021',
     project_name: 'Oh, abre este terreiro com arcos a enfeitá, chegou o Aruanda e nós vamos dançar',
     value: 'R$100.000,00 (cem mil reais)',
+    amendment: '40640022 | Autoria: Dep. Fed. Rogério Correia',
     accountability_date: 'fevereiro de 2025.',
     url: 'https://yornellas.github.io/aruanda-docs/Agreements/PUBLICACAO-TERMO-DE-FOMENTO-919860.pdf'
   },
@@ -20,6 +22,7 @@ export const Agreements = [
     signed_at: '14/11/2023',
     project_name: 'Vamos todos Nandaiá',
     value: 'R$100.000,00 (cem mil reais)',
+    amendment: '14080012 | Autoria: Dep. Fed. Patrus Ananias',
     accountability_date: '25 de dezembro de 2025.',
     url: 'https://yornellas.github.io/aruanda-docs/Agreements/PUBLICACAO-TERMO-DE-FOMENTO-947940.pdf'
   }
